6c5f247610e6508ab2b4719ca531e3279c6b5875
nikhathfirdose <nikhathfirdose@github.com>
2020-02-25 18:56:51 +0530

    sorting action complete

--- src/components/notes-list.jsx
+++ src/components/notes-list.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
 import firebase from "../firebase";
 
-function useNotes() {
+//sorting operation
+
+const SORT_OPTIONS = {
+  TITLE_ASC: { column: "title", direction: "asc" },
+  TITLE_DESC: { column: "title", direction: "desc" },
+  NOTE_DEFAULT: { column: "note", direction: "desc" }
+};
+function useNotes(sortBy = "NOTE_DEFAULT") {
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
     //todo:we need an unsubscribe callback
-    const unsubscribe = firebase; // the callback todo is done
-    firebase
+    const unsubscribe = firebase // the callback todo is done
       .firestore()
       .collection("notes")
+      .orderBy(SORT_OPTIONS[sortBy].column, SORT_OPTIONS[sortBy].direction)
       .onSnapshot(snapshot => {
         const newNotes = snapshot.docs.map(doc => ({
           id: doc.id,
@@ -19,21 +26,23 @@ function useNotes() {
         setNotes(newNotes);
       });
     return () => unsubscribe();
-  }, []);
+  }, [sortBy]);
+
   return notes;
 }
 
 const NoteList = () => {
-  const notes = useNotes();
+  const [sortBy, setSortBy] = useState("NOTE_DEFAULT");
+  const notes = useNotes(sortBy);
   return (
     <div>
       <h2>Notes List</h2>
       <div>
         <label>Sort by: </label>
-        <select>
-          <option>Latest</option>
-          <option>Alphabetical order</option>
-          <option>Disable Sort</option>
+        <select value={sortBy} onChange={e => setSortBy(e.currentTarget.value)}>
+          <option value="NOTE_DEFAULT"> Random </option>
+          <option value="TITLE_ASC">Title (a-z)</option>
+          <option value="TITLE_DESC">Title (z-a)</option>
         </select>
       </div>
       <ol>
