5143cd851579a387b5ca3d8f193c48db34540c8b
nikhathfirdose <nikhathfirdose@github.com>
2020-02-25 15:56:06 +0530

    initial working commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "./firebase";
+
+//this adds a "notes" data into firebase
+// firebase
+//   .firestore()
+//   .collection("notes")
+//   .add({
+//     noteTitle: "something",
+//     describe: "kk"
+//   });
+//totalyy creates a db, so ya we dont need this now
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Notes!</h1>
     </div>
   );
 }
